20d83a8ad88c2e36d3198608a7bb028c388a0b3e
Tony Mu <theveryshepherd@gmail.com>
2015-06-07 17:40:53 -0500

    added moneytracker stuff

---
+++ MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/MoneyTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoneyTracker
+//
+//  Created by Tony Mu on 6/3/15.
+//  Copyright (c) 2015 The Shepherd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
